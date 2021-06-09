fea30c2b2eadbccc92c56e05e22518de9a6363ab
Your Name <prottaybadhon54@gmail.com>
2021-06-10 05:13:16 +0600

    menu changed

--- src/Components/MenuSection.js
+++ src/Components/MenuSection.js
@@ -9,12 +9,12 @@ function MenuSection(props) {
                 <div class='row first_row'>
                     <div class='col-lg-6 col-md-6'>
                         <div class='row'>
-                            <div class='col-lg-6 col-md-12 col-lg-12'>
+                            <div class='col-lg-6 col-md-12'>
                             <div class='menu_img_div img-fluid'>
                                 <img src={food1} class=''></img>
                             </div>
                             </div>
-                            <div class='col-lg-6 py-5 col-md-12 col-lg-12'>
+                            <div class='col-lg-6 py-5 col-md-12 '>
                             <div class='menu_order_div px-3'>
                                    <h4>Grilled Beef with Potatoes </h4>
                                   <div class='dolar'>40$</div>
@@ -28,12 +28,12 @@ function MenuSection(props) {
 
                     <div class='col-lg-6 col-md-6'>
                         <div class='row'>
-                            <div class='col-lg-6 col-md-12 col-lg-12'>
+                            <div class='col-lg-6 col-md-12 '>
                             <div class='menu_img_div img-fluid'>
                                 <img src={food1} class=''></img>
                             </div>
                             </div>
-                            <div class='col-lg-6 py-5 col-md-12 col-lg-12'>
+                            <div class='col-lg-6 py-5 col-md-12 '>
                             <div class='menu_order_div px-3'>
                                    <h4>Grilled Beef with Potatoes </h4>
                                   <div class='dolar'>40$</div>
@@ -48,6 +48,88 @@ function MenuSection(props) {
                 </div>
 
 
+                <div class='row first_row'>
+                    <div class='col-lg-6 col-md-6'>
+                        <div class='row'>
+                            <div class='col-lg-6 col-md-12'>
+                            <div class='menu_img_div img-fluid'>
+                                <img src={food1} class=''></img>
+                            </div>
+                            </div>
+                            <div class='col-lg-6 py-5 col-md-12 '>
+                            <div class='menu_order_div px-3'>
+                                   <h4>Grilled Beef with Potatoes </h4>
+                                  <div class='dolar'>40$</div>
+                                <p>Meat, Potatoes, Rice, Tomatoe</p>
+                                <button class='btn'>Order now</button>
+                            </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+
+                    <div class='col-lg-6 col-md-6'>
+                        <div class='row'>
+                            <div class='col-lg-6 col-md-12 '>
+                            <div class='menu_img_div img-fluid'>
+                                <img src={food1} class=''></img>
+                            </div>
+                            </div>
+                            <div class='col-lg-6 py-5 col-md-12 '>
+                            <div class='menu_order_div px-3'>
+                                   <h4>Grilled Beef with Potatoes </h4>
+                                  <div class='dolar'>40$</div>
+                                <p>Meat, Potatoes, Rice, Tomatoe</p>
+                                <button class='btn'>Order now</button>
+                            </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                    
+                </div>
+
+
+                <div class='row first_row'>
+                    <div class='col-lg-6 col-md-6'>
+                        <div class='row'>
+                            <div class='col-lg-6 col-md-12'>
+                            <div class='menu_img_div img-fluid'>
+                                <img src={food1} class=''></img>
+                            </div>
+                            </div>
+                            <div class='col-lg-6 py-5 col-md-12 '>
+                            <div class='menu_order_div px-3'>
+                                   <h4>Grilled Beef with Potatoes </h4>
+                                  <div class='dolar'>40$</div>
+                                <p>Meat, Potatoes, Rice, Tomatoe</p>
+                                <button class='btn'>Order now</button>
+                            </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+
+                    <div class='col-lg-6 col-md-6'>
+                        <div class='row'>
+                            <div class='col-lg-6 col-md-12 '>
+                            <div class='menu_img_div img-fluid'>
+                                <img src={food1} class=''></img>
+                            </div>
+                            </div>
+                            <div class='col-lg-6 py-5 col-md-12 '>
+                            <div class='menu_order_div px-3'>
+                                   <h4>Grilled Beef with Potatoes </h4>
+                                  <div class='dolar'>40$</div>
+                                <p>Meat, Potatoes, Rice, Tomatoe</p>
+                                <button class='btn'>Order now</button>
+                            </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                    
+                </div>
                 
 
             
