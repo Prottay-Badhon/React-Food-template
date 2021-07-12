5cc3f22a2c144688145a9307fbbb79fc60f83ccb
Your Name <prottaybadhon54@gmail.com>
2021-07-12 06:51:12 +0600

    complete about page

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import "../css/page-css/AboutUs.css"
+import drinks1 from "../image/drinks1.jpg"
+import mango from "../video/mango.mp4" 
 function AboutUs(props) {
     return (
         <>
           <div class="aboutUsPage ">
+          
               <div class="row py-5">
 
                     <div class="col-lg-6 bg-light p-5">
@@ -30,11 +33,42 @@ function AboutUs(props) {
                                 </ul>
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                        
+                    <div class="col-lg-6 bg-dark text-white p-5">
+                        <div class="Delicious">
+                            <h1>A simple way to Eating Delicious</h1>
+                            <p>Land behold it created good saw after she'd Our set living. Signs midst 
+                                dominion creepeth morning 
+                                laboris nisi ufsit aliquip ex ea commodo conse quat is aute irure, 
+                                quis nostrud exer.
+                            </p>
+                            <button class="">Our Story</button>
+
+                            <h3 class="text-center mt-3">Quality is Our first Priority</h3>
+                            <img src={drinks1} class="img-fluid mt-4"></img>
+                        </div>
                     </div>
               </div>
-              
+
+              <video autoPlay loop controls muted responsive> 
+                  <source src={mango} type="video/mp4"></source>
+              </video>
+
+              <section  id="home-video" class="text-center text-light py-5">
+                <div class="dark-overlay">
+                    
+                        
+                        <div class="light-box" uk-lightbox>
+                            <a href="https://www.youtube.com/watch?v=yCCyJD7cVKU" class="text-danger">
+                            <i class="fa fa-play"></i>
+                            </a>
+                        <h2 class="mt-3 text-light">Restaurant in UK</h2>
+
+                        </div>
+                        </div>
+                   
+                  
+
+            </section>
             </div>  
         </>
     );
